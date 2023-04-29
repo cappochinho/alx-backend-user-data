e91b18ec997571d49649a98271d507e33975b97f
cappochinho <edwardprempeh18@gmail.com>
2023-04-29 19:06:10 +0100

    re: Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,10 +7,10 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
+                 separator: str) -> str:
     """Filters data using a regex"""
 
     for field in fields:
-        pattern = r'({0})=([^{1}]+)'.format(field, re.escape(separator))
-        message = re.sub(pattern, r'\1={}'.format(redaction), message)
+        pattern: str = r'({0})=([^{1}]+)'.format(field, re.escape(separator))
+        message: str = re.sub(pattern, r'\1={}'.format(redaction), message)
     return message
