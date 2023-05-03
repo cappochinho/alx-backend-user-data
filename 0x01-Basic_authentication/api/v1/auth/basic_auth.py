d5f7b022362f99f1e758dc156a2bc813ba14fee3
cappochinho <edwardprempeh18@gmail.com>
2023-05-03 07:54:07 +0100

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,21 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class for Basic Authentication
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extract a Base64 encoded string from the authorization header
+        """
+
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) != str:
+            return None
+
+        if authorization_header[0:6] != "Basic ":
+            return None
+
+        else:
+            return authorization_header[6:]
