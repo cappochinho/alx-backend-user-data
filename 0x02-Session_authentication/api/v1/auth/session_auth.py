1c9ee9807982a9ee33530fb9e2dab20720454cc2
cappochinho <edwardprempeh18@gmail.com>
2023-05-04 23:25:09 +0100

    Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for the Session Authentication class"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Contains the properties and methods of the
+    session authentication class
+    """
+
