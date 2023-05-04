a251e6a2dd31e7fb6b54108ca8d36c4e11f86786
cappochinho <edwardprempeh18@gmail.com>
2023-05-04 20:32:27 +0100

    added optional task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,11 +21,13 @@ class Auth():
         if excluded_paths is None or excluded_paths == []:
             return True
 
-        if path[len(path)-1] != '/':
-            path += '/'
-
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                prefix = excluded_path[:-1]
+                if path.startswith(prefix):
+                    return False
+            elif path == excluded_path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
