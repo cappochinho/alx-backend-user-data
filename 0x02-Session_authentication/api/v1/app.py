9da1ed41da597b99e934ebb52c95f6e408944694
cappochinho <edwardprempeh18@gmail.com>
2023-05-05 23:19:59 +0100

    Task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -67,16 +67,13 @@ def before_request_handler():
         '/api/v1/auth_session/login/'
     ]
     if path not in paths and not auth.require_auth(path, paths):
-        return
+        pass
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None\
+            and auth.session_cookie(request):
         abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
-
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
     request.current_user = auth.current_user(request)
 
 
