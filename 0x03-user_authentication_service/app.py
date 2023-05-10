64fd242049ac8f2e8972f701265e90258c5528e6
cappochinho <edwardprempeh18@gmail.com>
2023-05-11 00:32:17 +0100

    Task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,7 +55,7 @@ def logout():
     """logout of a session
     """
 
-    session_id = request.cookies.get("session_id")
+    session_id = request.form.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user)
