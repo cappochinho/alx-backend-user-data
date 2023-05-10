9b63ec4ff77e255a008df075e35541529ec11441
cappochinho <edwardprempeh18@gmail.com>
2023-05-11 00:38:58 +0100

    Task 14: Fix 2

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,7 +56,7 @@ def logout():
     """
 
     session_id = request.cookies.get("session_id")
-    if not session_id or AUTH.get_user_from_session_id(session_id):
+    if not session_id or not AUTH.get_user_from_session_id(session_id):
         abort(403)
     AUTH.destroy_session(session_id)
     return redirect('/')
